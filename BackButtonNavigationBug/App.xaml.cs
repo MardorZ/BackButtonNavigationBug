d9f4742d6675176cf88b55531593609482d21a8e
Dave Kinne <kinne@vital-services.de>
2023-03-19 10:52:09 +0100

    Projektdateien hinzufügen.

---
+++ BackButtonNavigationBug/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BackButtonNavigationBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ BackButtonNavigationBug/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace BackButtonNavigationBug
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private INavigationService _navigationService;
+
+        public MainPage(MainViewModel viewModel, INavigationService navigationService)
+        {
+            this.InitializeComponent();
+            this.BindingContext = viewModel;
+            this._navigationService = navigationService;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await _navigationService.NavigateAsync("DetailPage");
+        }
+    }
+}
